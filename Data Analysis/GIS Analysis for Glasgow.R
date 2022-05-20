863833c2039abcecf09a22b42c58fe38a254b191
dataandcrowd <shinhyesop@gmail.com>
2022-05-20 07:22:56 +0100

    update r stats

--- Data Analysis/GIS Analysis for Glasgow.R
+++ Data Analysis/GIS Analysis for Glasgow.R
@@ -1,9 +1,17 @@
 library(tidyverse)
 library(sf)
 
-schools <- read_sf("Glasgow/Bld_Schools_Glasgow.shp")
+schools <- 
+  read_sf("Glasgow/Bld_Schools_Glasgow.shp") %>% 
+  select(-c(FID_1, FID_2)) %>% 
+  mutate(FID = row_number()) %>% 
+  select(FID, 2:15) # display a few variables
 iz <- read_sf("Glasgow/Glasgow_IZ.shp")
 building <- read_sf("Glasgow/Bld_Glasgow.shx") %>% select(zone, area, type)
 pgarden <- read_sf("Glasgow/Private_Garden_Glasgow.shp")
 
 # I need to do a spatial join to get a zonal stats
+# 
+# sptial join and sum
+aggregate(iz[1], schools[1], sum)
+https://ryanpeek.org/2019-04-29-spatial-joins-in-r/
