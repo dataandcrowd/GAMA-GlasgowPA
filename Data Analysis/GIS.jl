a68449e4254594f609b9d5dfe9583b04230a729a
Hyesop <shinhyesop@gmail.com>
2022-05-20 21:19:37 +0100

    Create GIS.jl

---
+++ Data Analysis/GIS.jl
@@ -0,0 +1,14 @@
+# use package
+#using Pkg
+#Pkg.add("GDAL")
+#Pkg.build("GDAL") 
+#Pkg.add("ArchGDAL") 
+#Pkg.add("Plots")
+#Pkg.add("Shapefile")
+using ArchGDAL 
+const AG = ArchGDAL
+using Plots
+
+#import GeoDataFrames as GDF
+#df = GDF.read("Glasgow/Glasgow_IZ.shp")
+
