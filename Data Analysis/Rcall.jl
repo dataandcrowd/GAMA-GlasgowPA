39f8edf03cb8ec87dd2c68a9c06e9e3b3508c348
Hyesop <shinhyesop@gmail.com>
2022-06-01 16:04:21 +0100

    Update Rcall.jl

--- Data Analysis/Rcall.jl
+++ Data Analysis/Rcall.jl
@@ -1,14 +1,14 @@
 ENV["R_HOME"] = "C:\\Program Files\\R\\R-4.1.3" ## Windows
-ENV["R_HOME"] = "/Library/Frameworks/R.framework/Resources" ##MacOSX
+#ENV["R_HOME"] = "/Library/Frameworks/R.framework/Resources" ##MacOSX
 
 using Pkg
-#Pkg.add("RCall") #<- Essential
-#Pkg.build("RCall") #<- Essential
 using RCall
-
-#ggplot(diamonds, aes(x=carat, y=price)) + geom_point() +  theme_bw()
+using Plots
 
 @rlibrary sf
 #R"library(dplyr)"
+R"library(sf)"
 
-#nc = R"st_read(system.file("shape/nc.shp")"
+
+#https://youtu.be/yPaEB6er2Iw
+
